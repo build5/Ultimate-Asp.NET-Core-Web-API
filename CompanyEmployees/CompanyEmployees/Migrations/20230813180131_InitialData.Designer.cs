b421dd15fb5884915b85785b39d68690e30c5657
build5 <104657680+build5@users.noreply.github.com>
2023-08-13 23:59:17 +0330

    Implement rest of chapter 3
    Add some data to database to have data to work with.
    Add Repository pattern logic by creating Repository project.
    Add interfaces for repository logic in Contracts project.
    Implement repository interfaces in Repository project.
    Add service layer by creating Service.Contracts and Service projects.
    Add service layer interfaces in Service.Contracts project.
    Implement service layer interfaces in Service projects.
    Configure repository and service logic in ServiceExtensions and Program.cs of the main project.

--- CompanyEmployees/CompanyEmployees/Migrations/20230813180131_InitialData.Designer.cs
+++ CompanyEmployees/CompanyEmployees/Migrations/20230813180131_InitialData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Repository;
@@ -11,9 +12,11 @@ using Repository;
 namespace CompanyEmployees.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20230813180131_InitialData")]
+    partial class InitialData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
