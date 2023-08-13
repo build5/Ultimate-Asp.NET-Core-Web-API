795996652c3768667a55472e47fe676c6fda3f14
build5 <104657680+build5@users.noreply.github.com>
2023-08-13 19:13:58 +0330

    Implement Chapter 1 of the book

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -1,14 +1,28 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+}); ;
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
