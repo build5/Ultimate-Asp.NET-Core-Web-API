e15d28f478906e4f302afa16a683799f130f0f79
build5 <104657680+build5@users.noreply.github.com>
2023-08-14 21:53:47 +0330

    Implement parts of Chapter 6
    Add GetCompany to retrieve a single Company to the client action in CompaniesController.
    Add GetCompany logic in repository and service layer.
    Add NotFoundException in Entities project.
    Update ExceptionMiddlewareExtensions to use CompanyNotFoundExceptions

--- CompanyEmployees/Repository/CompanyRepository.cs
+++ CompanyEmployees/Repository/CompanyRepository.cs
@@ -11,5 +11,8 @@ internal sealed class CompanyRepository : RepositoryBase<Company>, ICompanyRepos
 
 	public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
 		FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+
+	public Company GetCompany(Guid companyId, bool trackChanges) =>
+		FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
 }
 
