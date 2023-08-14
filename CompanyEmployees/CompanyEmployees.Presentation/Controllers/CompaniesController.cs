06d227f9e8742e056b66206a79e7044c34a2cd94
build5 <104657680+build5@users.noreply.github.com>
2023-08-14 21:26:03 +0330

    Implement Chapter 5
    Global Error Handling
    Add ErrorModel in Entities project.
    Add ExceptionMiddlewareExtensions.
    Update service layer to use global error handling.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -10,22 +10,15 @@ public class CompaniesController : ControllerBase
     private readonly IServiceManager _service;
 
     public CompaniesController(IServiceManager service)
-	{
-		_service = service;
-	}
+    {
+        _service = service;
+    }
 
-	[HttpGet]
-	public IActionResult GetCompanies()
-	{
-		try
-		{
-			var companies = _service.CompanyService.GetAllCompanies(trackChanges: false);
-			return Ok(companies);
-		}
-		catch
-		{
-			return StatusCode(500, "Internal server error.");
-		}
-	}
+    [HttpGet]
+    public IActionResult GetCompanies()
+    {
+        var companies = _service.CompanyService.GetAllCompanies(trackChanges: false);
+        return Ok(companies);
+    }
 }
 
--- CompanyEmployees/Service/CompanyService.cs
+++ CompanyEmployees/Service/CompanyService.cs
@@ -12,25 +12,17 @@ internal sealed class CompanyService : ICompanyService
     private readonly IMapper _mapper;
 
     public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
-	{
-		_repository = repository;
-		_logger = logger;
+    {
+        _repository = repository;
+        _logger = logger;
         _mapper = mapper;
-	}
+    }
 
     public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
     {
-        try
-        {
-            var companies = _repository.Company.GetAllCompanies(trackChanges);
-            var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
-            return companiesDto;
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex}");
-            throw;
-        }
+        var companies = _repository.Company.GetAllCompanies(trackChanges);
+        var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+        return companiesDto;
     }
 }
 
