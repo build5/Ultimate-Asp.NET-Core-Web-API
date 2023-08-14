b0a46e89cfb67ab505a078110a5da512b34c9302
build5 <104657680+build5@users.noreply.github.com>
2023-08-14 19:41:45 +0330

    Implement rest of Chapter 4
    Install AutoMapper in Service project and register it in the main project.
    Add Shared project to hold Dtos.
    Add MappingProfile in the main project and update service layer logic to use AutoMapper and CompanyDto

--- CompanyEmployees/Service/CompanyService.cs
+++ CompanyEmployees/Service/CompanyService.cs
@@ -1,6 +1,7 @@
-﻿using Contracts;
-using Entities.Models;
+﻿using AutoMapper;
+using Contracts;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service;
 
@@ -8,19 +9,22 @@ internal sealed class CompanyService : ICompanyService
 {
     private readonly IRepositoryManager _repository;
     private readonly ILoggerManager _logger;
+    private readonly IMapper _mapper;
 
-    public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+    public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
 	{
 		_repository = repository;
 		_logger = logger;
+        _mapper = mapper;
 	}
 
-    public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+    public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
     {
         try
         {
             var companies = _repository.Company.GetAllCompanies(trackChanges);
-            return companies;
+            var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+            return companiesDto;
         }
         catch (Exception ex)
         {
