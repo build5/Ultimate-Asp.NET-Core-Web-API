18f15435a3e7a3de1d51144adc2040bda4edd7f0
build5 <104657680+build5@users.noreply.github.com>
2023-08-15 23:27:07 +0330

    Implement rest of chapter 9
    Add GetCompanyCollection and CreateCompanyCollection actions.
    Add GetByIds in repository layer for getting collection of companies.
    Add GetByIds and CreateCompanyCollection logic in service layer.
    Add BadRequestException logic and update ExceptionMiddlewareExtensions.
    Add ArrayModelBinder in presentation project to get collection of guis and return string collection.
    Modify CompanyForCreationDto to be able to create a company with employees in one action.

---
+++ CompanyEmployees/CompanyEmployees.Presentation/ModelBinders/ArrayModelBinder.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CompanyEmployees.Presentation.ModelBinders;
+
+public class ArrayModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        if (!bindingContext.ModelMetadata.IsEnumerableType)
+        {
+            bindingContext.Result = ModelBindingResult.Failed();
+            return Task.CompletedTask;
+        }
+
+        var providedValue = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+
+        if (string.IsNullOrEmpty(providedValue))
+        {
+            bindingContext.Result = ModelBindingResult.Success(null);
+            return Task.CompletedTask;
+        }
+
+        var genericType = bindingContext.ModelType.GetTypeInfo().GenericTypeArguments[0];
+        var converter = TypeDescriptor.GetConverter(genericType);
+
+        var objectArray = providedValue.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => converter.ConvertFromString(x.Trim())).ToArray();
+
+        var guideArray = Array.CreateInstance(genericType, objectArray.Length);
+        objectArray.CopyTo(guideArray, 0);
+        bindingContext.Model = guideArray;
+
+        bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+        return Task.CompletedTask;
+    }
+}
+
--- CompanyEmployees/Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ CompanyEmployees/Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -1,4 +1,5 @@
 ﻿namespace Shared.DataTransferObjects;
 
-public record CompanyForCreationDto(string Name, string Address, string Country);
+public record CompanyForCreationDto(string Name, string Address, string Country,
+    IEnumerable<EmployeeForCreationDto> Employees);
 
