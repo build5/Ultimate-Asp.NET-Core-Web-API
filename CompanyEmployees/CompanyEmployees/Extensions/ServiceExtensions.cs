ef18af379aa28100ec8ab1dd4976e9d38f21c1a8
build5 <104657680+build5@users.noreply.github.com>
2023-08-15 04:35:18 +0330

    Implement Chapter 7
    Add content negotiation for xml response for CompanyDto.
    Add custom negotiation for csv response for CompanyDto.

--- CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -34,5 +34,8 @@ public static class ServiceExtensions
 
     public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
         services.AddDbContext<RepositoryContext>(opts => opts.UseNpgsql(configuration.GetConnectionString("sqlConnection")));
+
+    public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+        builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
 }
 
