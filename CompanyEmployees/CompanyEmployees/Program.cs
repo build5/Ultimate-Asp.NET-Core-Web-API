124083d699e93cb286a880bd722f8f0258d0e0b5
build5 <104657680+build5@users.noreply.github.com>
2023-08-18 23:53:51 +0330

    Implement chapter 15
    Add action filters to CompaniesController.

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CompanyEmployees.Extensions;
+using CompanyEmployees.Presentation.ActionFilters;
 using Contracts;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +25,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
 });
+builder.Services.AddScoped<ValidationFilterAttribute>();
 
 builder.Services.AddControllers(config =>
 {
