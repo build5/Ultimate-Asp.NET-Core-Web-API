72ae8466b97ef11411edb1f3636df9f74d83f9db
build5 <104657680+build5@users.noreply.github.com>
2023-08-19 21:58:59 +0330

    Implement chapter 17
    Add filters for Age field for employee entity.

--- CompanyEmployees/Service/EmployeeService.cs
+++ CompanyEmployees/Service/EmployeeService.cs
@@ -23,6 +23,8 @@ internal sealed class EmployeeService : IEmployeeService
 
 	public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
 	{
+		if (!employeeParameters.ValidAgeRange)
+			throw new MaxAgeRangeBadRequestException();
 		await CheckIfCompanyExists(companyId, trackChanges);
 		var employeesWithMetaData = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges);
 		var employeeDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesWithMetaData);
